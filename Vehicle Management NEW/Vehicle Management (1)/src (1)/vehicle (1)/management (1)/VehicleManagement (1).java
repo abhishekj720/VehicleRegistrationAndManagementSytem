0b9045189737dff78939f257fcaeeb6d6af7face
Abhishek Jha <abhishekj720@gmail.com>
2018-04-21 20:44:32 +0530

    submit
    upload of project one

---
+++ Vehicle Management NEW/Vehicle Management (1)/src (1)/vehicle (1)/management (1)/VehicleManagement (1).java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicle.management;
+
+/**
+ *
+ * @author Ankit Akash
+ */
+public class VehicleManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
